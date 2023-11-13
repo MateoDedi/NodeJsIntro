838829d927842390708f7e4c4af05590a99c02b0
MateoDedi <mateod@live.com>
2023-11-13 18:29:08 +0100

    .

--- Server01.js
+++ Server01.js
@@ -2,17 +2,44 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-type", "text/html");
+  if (req.url === "/") {
 
-  fs.readFile("./views/index.html", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
+    fs.readFile("./views/index.html", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (req.url === "/about") {
+    fs.readFile("./views/about.html", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end;
+      } else {
+        res.write(data);
+        res.statusCode = 200;
+        res.end;
+      }
+    });
+  } else if (req.url === "/about-us") {
+    res.setHeader("Location", "/about");
+    res.statusCode = 301;
+    res.end();
+  } else {
+    fs.readFile("./views/404.html", (err, data) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        res.write(data);
+        res.statusCode = 404;
+        res.end();
+      }
+    });
+  }
 });
 
 server.listen("3000", "localhost", () => {
